83c2e2413b157aa5568d2c14e61c4ce5d3750c31
Sayantan De <desayantan1947@gmail.com>
2025-10-22 16:50:44 +0530

    updated host

--- evault/settings.py
+++ evault/settings.py
@@ -181,16 +181,6 @@ SESSION_CACHE_ALIAS = "default"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 
