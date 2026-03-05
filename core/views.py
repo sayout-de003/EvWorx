98c40621446d904dacf7ae5002ef09e2a2a8c522
Sayantan De <desayantan1947@gmail.com>
2026-03-05 13:46:15 +0530

    fix: added missing reverse import and fixed JS syntax errors in dashboard

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
